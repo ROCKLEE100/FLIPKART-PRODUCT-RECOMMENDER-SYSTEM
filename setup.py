68566ae76f088600586f7d40cafdd68d92d6696c
ROCKLEE100 <adityarathod7799@github.com>
2025-09-29 12:03:59 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+
+    name="FLIPKART RECOMMENDER",
+    version="0.1",
+    author="Aditya",
+    packages=find_packages(),
+    install_requires = requirements,
+
+
+)
